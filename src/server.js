9a397147ba6c5e6f63e2c9cfa9bd95c302ad6b7a
welitonwt <weliton.ww@hotmail.com>
2021-12-29 22:01:35 -0300

    fizemos um servidor para o projeto

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const server = express()
+server.get('/', (request, response) => {
+
+    return response.sendFile(__dirname + "/views/index.html")
+
+})
+server.listen(3000, ()=> console.log('rodando'))
+
