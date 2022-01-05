16954db8dd11c8cd1f963678700506648b6e5b1c
welitonwt <weliton.ww@hotmail.com>
2022-01-05 20:49:37 -0300

    object Profile conclui os calculos das horas e valores dos trabalhos

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,54 @@ const routes = express.Router()
 const views = __dirname + "/views/"
 
 
-const profile = {
+const Profile = {
 
-    name: "Weliton",
-    avatar: "https://avatars.githubusercontent.com/u/95891521?v=4",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4,
-    "value-hour": 75
-    
+    data:{
 
+        name: "Weliton",
+        avatar: "https://avatars.githubusercontent.com/u/95891521?v=4",
+        "monthly-budget": 3000,
+        "days-per-week": 5,
+        "hours-per-day": 5,
+        "vacation-per-year": 4,
+        "value-hour": 75
+    },
+
+    controllers:{
+        index(req, res) {
+           return res.render(views + "profile", { profile: Profile.data })
+        },
+
+        update(req, res){
+
+            // req.body para os dados
+            const data = req.body
+            
+            // definir quantas semanas tem num ano
+            const weeksPerYear = 52
+
+            // remover as semanas de ferias do ano
+            const weeksPerMonth = (weeksPerYear - data ["vacation-per-year"]) / 12
+
+            // quantas horas por semana estou trabalhando
+            const weekTotalHours = data["hours-per-day"] * data["days-per-week"] 
+
+            // total de horas trabalhadas no mes
+            const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+            // qual sera o valor da minha hora ?
+            const valueHour =  data["value-hour"] = data["monthly-budget"] / monthlyTotalHours
+
+            Profile.data = {
+                ...Profile.data,
+                ...req.body,
+                "value-hour": valueHour
+            }
+
+            return res.redirect('/profile')
+
+        }
+    }
 }
 
 const Job = {
@@ -50,7 +87,7 @@ const Job = {
                         ...job,
                         remaining,
                         status,
-                        budget: profile["value-hour"] * job["total-hours"]
+                        budget: Profile.data["value-hour"] * job["total-hours"]
             
                     }
                 })
@@ -58,12 +95,15 @@ const Job = {
                return res.render(views + "index", {jobs: updatedJobs})            
             },
 
-
         create(req, res) {
+           return res.render(views + "job")
+        },
+
+        save(req, res) {
 
             const lastId = Job.data[Job.data.length - 1]?.id || 1;
             
-            Job.push({
+            Job.data.push({
                 id: lastId + 1,
                 name: req.body.name,
                 "daily-hours": req.body["daily-hours"],
@@ -104,12 +144,14 @@ const Job = {
 
 
 routes.get('/', Job.controllers.index)
-
-routes.get('/job', (req, res) => res.render(views + "job"))
-routes.post('/job', Job.controllers.create)
+routes.get('/job', Job.controllers.create)
+routes.post('/job', Job.controllers.save)
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+routes.get('/profile', Profile.controllers.index)
+routes.post('/profile', Profile.controllers.update)
 
 
 
 module.exports = routes;
+
+
