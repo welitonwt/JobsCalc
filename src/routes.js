7c807d11a2774caad5be5885d024815e0e145a37
welitonwt <weliton.ww@hotmail.com>
2022-01-05 22:36:21 -0300

    editar os jobs

--- src/routes.js
+++ src/routes.js
@@ -57,49 +57,52 @@ const Profile = {
 
 const Job = {
 
-    data:[
+  data:[
         
-            {
-                id: 1,
-                name: "Pizzaria Guloso",
-                "daily-hours": 2,
-                "total-hours": 1,
-                created_at: Date.now()
-            },
-            {
-                id: 2,
-                name: "OneTwo Project",
-                "daily-hours": 3,
-                "total-hours": 47,
-                created_at: Date.now() 
-            }
-        
-    ],
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 1,
+        created_at: Date.now(),
 
-    controllers: {
-        index(req, res){    
-                const updatedJobs = Job.data.map((job) => {
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        "total-hours": 47,
+        created_at: Date.now(),
+
+    }
+        
+ ],
+
+ controllers: {
+    index(req, res){    
+        const updatedJobs = Job.data.map((job) => {
             
-                    const remaining = Job.services.remainingDays(job)
-                    const status = remaining <= 0 ? 'done' : 'progress'
-            
-                    return {
-                        ...job,
-                        remaining,
-                        status,
-                        budget: Profile.data["value-hour"] * job["total-hours"]
+        const remaining = Job.services.remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
+           
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
             
                     }
-                })
+            })
                     
-               return res.render(views + "index", {jobs: updatedJobs})            
-            },
+         return res.render(views + "index", {jobs: updatedJobs})
 
-        create(req, res) {
+        },
+
+    create(req, res) {
            return res.render(views + "job")
         },
 
-        save(req, res) {
+    save(req, res) {
 
             const lastId = Job.data[Job.data.length - 1]?.id || 1;
             
@@ -115,8 +118,62 @@ const Job = {
         
             return res.redirect('/')
         
-        }
         },
+
+    show(req, res){
+
+
+        const jobId = req.params.id
+
+        const job = Job.data.find( job => Number(job.id) === Number(jobId))
+
+        if(!job){
+            
+            return res.send('Job not found!')
+
+        }
+
+            job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+
+            return res.render(views + "job-edit", { job })
+    },
+    
+    update(req, res) {
+
+        //peguei os parametros
+        const jobId = req.params.id
+
+        //estou procurando o jod dentro dos jods
+        const job = Job.data.find( job => Number(job.id) === Number(jobId))
+
+        // senao encontrar eu dou esse alert
+        if(!job){
+            
+            return res.send('Job not found!')
+
+        }
+
+        const updatedJob = {
+            ...job, 
+            name: req.body.name,
+            "total-hours": req.body["total-hours"],
+            "daily-hours": req.body["daily-hours"],
+        }
+
+        Job.data  = Job.data.map(job =>{
+
+            if(Number(job.id) === Number(jobId)) {
+                job = updatedJob
+            }
+
+            return job
+        })
+
+        res.redirect('/job/' + jobId)
+    }
+
+
+    },
 
         services:{
             remainingDays(job){
@@ -136,9 +193,11 @@ const Job = {
             // restam x dias
                 return dayDiff
             
-            }
+            },
+
+            calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
         }
-    }
+}
 
 
 
@@ -146,7 +205,8 @@ const Job = {
 routes.get('/', Job.controllers.index)
 routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/job/:id',Job.controllers.show)
+routes.post('/job/:id',Job.controllers.update)
 routes.get('/profile', Profile.controllers.index)
 routes.post('/profile', Profile.controllers.update)
 
